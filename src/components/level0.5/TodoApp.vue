75c0d1d025bdc49f0d083f00801cc486ed0ab64b
Yuri Alcantara <yuri.alcantara1995@gmail.com>
2022-11-21 16:26:06 +0100

    Level 1 Tests passed

--- src/components/level0.5/TodoApp.vue
+++ src/components/level0.5/TodoApp.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 
+const newTodo = ref('');
+
 const todos = ref([
   {
     id: 1,
@@ -8,7 +10,26 @@ const todos = ref([
     completed: false,
   },
 ]);
+
+function addTodo() {
+  let id = todos.value.length + 1;
+  todos.value.push({
+    id: id,
+    text: newTodo.value,
+    completed: false
+  })
+}
+
 </script>
 <template>
-  <div></div>
+  <input v-model="newTodo" type="text" data-test="new-todo-input"/>
+  <button @click="addTodo()" data-test="new-todo-btn">Create Todo</button>
+  <div>
+    <ul>
+      <li data-test="todo" v-for="todo in todos" :class="{completed: todo.completed}">
+        <input type="checkbox" data-test="todo-checkbox" v-model="todo.completed">
+        {{todo.text}}
+      </li>
+    </ul>
+  </div>
 </template>
